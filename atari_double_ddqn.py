cb389d84d486d92d2c9c826cda767afefab776f5
acez <aczjj@qq.com>
2021-12-06 14:17:42 +0800

    fix the bug of no detach for target model

--- atari_double_ddqn.py
+++ atari_double_ddqn.py
@@ -19,8 +19,8 @@ class CnnDoubleDQNAgent(CnnDDQNAgent):
         # different from DQN
         # double DQN use model (not target model) to choose next actions
         next_a = self.model(s1).max(1)[1].view(-1, 1)
-        # use the target network to estimate next Q
-        q_values_next = self.target_model(s1).gather(1, next_a)
+        # use the target network to estimate next Q (IMPORTANT: no grad.)
+        q_values_next = self.target_model(s1).detach().gather(1, next_a)
         # target q values
         q_values_tar = r + self.config.gamma * q_values_next * (1-done)
         # mse loss
--- atari_prioritized_ddqn.py
+++ atari_prioritized_ddqn.py
@@ -37,8 +37,8 @@ class CnnPrioritizedDDQNAgent(CnnDDQNAgent):
 
         # calculate TD loss
         q_values = self.model(s0).gather(1, a)
-        # use the target network to estimate next Q
-        q_values_next = self.target_model(s1).max(1)[0].view(-1, 1)
+        # use the target network to estimate next Q (IMPORTANT: no grad.)
+        q_values_next = self.target_model(s1).detach().max(1)[0].view(-1, 1)
         # target q values
         q_values_tar = r + self.config.gamma * q_values_next * (1-done)
         td_err = q_values_tar - q_values
